7dfbd2b7bba8b2b91798e278373f9c3ac51bbf7d
ldcss <lucass.dcss75@gmail.com>
2025-05-28 22:40:36 -0300

    feat: add swagger, class validator and transform

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  // Configuração do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Minha API de Compras')
+    .setDescription('Documentação da API do sistema de compras online')
+    .setVersion('1.0')
+    .addTag('user', 'Operações relacionadas a usuários')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
