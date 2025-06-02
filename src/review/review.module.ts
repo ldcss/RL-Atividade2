cef235a5ea3c29e8e2cf8dcfc24b017fe8f0334f
ldcss <lucass.dcss75@gmail.com>
2025-06-01 23:13:55 -0300

    fix: import necessary modules in ReviewModule

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ReviewService } from './review.service';
 import { ReviewController } from './review.controller';
-
+import { PrismaModule } from 'prisma/prisma.module';
+import { UserModule } from 'src/user/user.module';
+import { ProductModule } from 'src/product/product.module';
 @Module({
+  imports: [PrismaModule, UserModule, ProductModule],
   controllers: [ReviewController],
   providers: [ReviewService],
 })
